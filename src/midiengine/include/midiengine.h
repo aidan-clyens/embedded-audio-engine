a6dba2b74cf9b7acae154dc03a7168ce0e5bb32c
Aidan Clyens <aidanclyens@gmail.com>
2025-06-19 00:06:09 +0100

    Add MIDI Engine template

---
+++ src/midiengine/include/midiengine.h
@@ -0,0 +1,14 @@
+#ifndef _MIDI_ENGINE_H
+#define _MIDI_ENGINE_H
+
+namespace MidiEngine
+{
+
+  class MidiEngine
+  {
+   
+  };
+
+}  // namespace MidiEngine
+
+#endif  // _MIDI_ENGINE_H
---
+++ src/midiengine/src/midiengine.cpp
@@ -0,0 +1,1 @@
+#include "midiengine.h"
