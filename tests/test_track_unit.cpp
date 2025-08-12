d8dfcab61e2d5baa8edb21b85e0f5cfd6d99c4f9
Aidan Clyens <aidanc@rogers.com>
2025-08-12 00:35:26 -0400

    Add Track unit tests

---
+++ tests/test_track_unit.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include <iostream>
+
+#include "trackmanager.h"
+#include "track.h"
+
+using namespace Tracks;
+
+/** @brief Track - Add Audio Input
+ */
+TEST(TrackTest, AddAudioInput)
+{
+  EXPECT_TRUE(false) << "This test is not implemented yet.";
+}
+
+/** @brief Track - Add MIDI Input
+ */
+TEST(TrackTest, AddMidiInput)
+{
+  EXPECT_TRUE(false) << "This test is not implemented yet.";
+}
+
+/** @brief Track - Add Audio Output
+ */
+TEST(TrackTest, AddAudioOutput)
+{
+  EXPECT_TRUE(false) << "This test is not implemented yet.";
+}
